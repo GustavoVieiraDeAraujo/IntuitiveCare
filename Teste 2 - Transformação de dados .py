54228d2b90ad7f06cbe8922a49791237131c1c03
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2022-06-01 15:31:09 -0300

    Descompactando 'Anexos.zip' e convertendo o 'Anexo I.pdf' em 'convertido.csv'

--- Teste 2 - Transformação de dados .py
+++ Teste 2 - Transformação de dados .py
@@ -1,5 +1,6 @@
 #Bibliotecas utilizadas
 import os
+import tabula
 from zipfile import ZipFile, ZIP_DEFLATED
 
 #Função para descompactar um ZIP
@@ -14,3 +15,9 @@ def ziparArquivoDeUmaPasta(pasta, nomeDoArquivoDentroDaPasta, nomeDoArquivoZipad
     arquivoZipado.write(os.path.join(pasta, listaArquivos[listaArquivos.index(
         nomeDoArquivoDentroDaPasta)]),  listaArquivos[listaArquivos.index(nomeDoArquivoDentroDaPasta)])
     arquivoZipado.close()
+
+#Descompactando "Anexos.zip" na pasta "temporario"
+descompactarZip("Anexos.zip", "./temporario")
+
+#Converter "Anexo I.pdf" em "convertido.csv"
+tabula.convert_into("./temporario/Anexo I.pdf","./temporario/convertido.csv", output_format="csv", pages="3-200")
