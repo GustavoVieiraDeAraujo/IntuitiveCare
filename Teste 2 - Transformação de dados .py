2a490fda83638534c9aeb8dde3cb4e6dab4ea3f7
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2022-06-01 15:16:32 -0300

    Criando função ziparArquivoDeUmaPasta

--- Teste 2 - Transformação de dados .py
+++ Teste 2 - Transformação de dados .py
@@ -1,4 +1,5 @@
 #Bibliotecas utilizadas
+import os
 from zipfile import ZipFile, ZIP_DEFLATED
 
 #Função para descompactar um ZIP
@@ -6,4 +7,10 @@ def descompactarZip(arquivo, ondeDescompactar):
     with ZipFile(arquivo, 'r') as zip:
         zip.extractall(ondeDescompactar)
 
-
+#Função para zipar um arquivo
+def ziparArquivoDeUmaPasta(pasta, nomeDoArquivoDentroDaPasta, nomeDoArquivoZipado):
+    listaArquivos = os.listdir(pasta)
+    arquivoZipado = ZipFile(nomeDoArquivoZipado, "a", compression=ZIP_DEFLATED)
+    arquivoZipado.write(os.path.join(pasta, listaArquivos[listaArquivos.index(
+        nomeDoArquivoDentroDaPasta)]),  listaArquivos[listaArquivos.index(nomeDoArquivoDentroDaPasta)])
+    arquivoZipado.close()
