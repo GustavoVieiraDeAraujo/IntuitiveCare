65fc5f3800d604855e02dee9f344fac34edc5766
Gustavo Vieira de Araújo <gustavova2003@hotmail.com>
2022-06-01 15:49:00 -0300

    Criando função formatarLinha, alem do arquivo 'Anexo I.csv'

--- Teste 2 - Transformação de dados .py
+++ Teste 2 - Transformação de dados .py
@@ -1,7 +1,15 @@
 #Bibliotecas utilizadas
 import os
+import csv
 import tabula
 from zipfile import ZipFile, ZIP_DEFLATED
+
+#Função para formatar linhas do arquivo CSV
+def formatarLinha(linha):
+    for i in linha:
+        if "\n" in i:
+            linha[linha.index(i)] = i.replace("\n", " ")
+    return linha
 
 #Função para descompactar um ZIP
 def descompactarZip(arquivo, ondeDescompactar):
@@ -21,3 +29,16 @@ descompactarZip("Anexos.zip", "./temporario")
 
 #Converter "Anexo I.pdf" em "convertido.csv"
 tabula.convert_into("./temporario/Anexo I.pdf","./temporario/convertido.csv", output_format="csv", pages="3-200")
+
+#Gerando "Anexo I.csv" utilizando "convertido.csv"
+with open("./temporario/convertido.csv", "r", encoding='utf-8') as convertido:
+    with open("./temporario/Anexo I.csv", 'w', encoding='utf-8') as formatado:
+        matriz_csv_file = csv.reader(convertido)
+        csv.writer(formatado, delimiter=',').writerow(['PROCEDIMENTO', 'RN (alteração)', 'VIGÊNCIA', 'Seg. Odontológica',
+                                                       'Seg. Ambulatorial', 'HCO', 'HSO', 'REF', 'PAC', 'DUT', 'SUBGRUPO', 'GRUPO', 'CAPÍTULO'])
+        for linha in matriz_csv_file:
+            if linha[0] == "PROCEDIMENTO":
+                pass
+            else:
+                csv.writer(formatado, delimiter=',').writerow(
+                    formatarLinha(linha))
